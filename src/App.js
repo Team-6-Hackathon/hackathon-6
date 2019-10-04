5ce2b00c689cb8449048f339e97be41398c67d6b
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-10-04 11:46:42 -0400

    Installed new dependencies.  Added Navbar styled by Bulma.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from "react-router-dom"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Making changes</h1>
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+class Navbar extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <nav className = "navbar">
+        <div className = "navbar-brand">
+          <img src= "http://static.dezeen.com/uploads/2014/07/Airbnb-rebrand-by-DesignStudio_dezeen_468_8.jpg"/>
+        </div>
+        <div className = "navbar-menu">
+          <div className = "navbar-start">
+            <div className="field">
+              <div className="control has-icons-left">
+                <input id = "search-bar" className="input" type="text" placeholder="Paris" />
+                <span className="icon is-small is-left">
+                  <img src ="/magnifying_glass.svg" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className = "navbar-end">
+            <ul className = "nav-links">
+              <li><Link to='/signup'>Sign up</Link></li>
+              <li><Link to='/login'>Log in</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      )
+  }
+}
+
+
+export default Navbar
