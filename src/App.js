41f579a18ddc4b8d5f41470f31d42e351a0b8272
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-10-04 12:08:59 -0400

    Setup Switch and Routes (Uncomment when done)

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import {Switch, Route} from "react-router-dom"
 import Navbar from './components/Navbar';
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Switch>
+        {/* <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} /> */}
+        <Route exact path='/search' component={Search} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Result.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <img src= "https://i.pinimg.com/originals/de/d5/be/ded5be8e4632b8347cfc5716b0b3062d.jpg"/>
+        <div>
+          <h3>Listing Type</h3>
+          <h3>Listing Name</h3>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Search
